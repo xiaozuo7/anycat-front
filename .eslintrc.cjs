8359a4755937eaddee1276b8fbe92c28abf0fee3
xiaozuo7 <893985729@qq.com>
2024-02-27 18:19:47 +0800

    [init]init

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,25 +1,32 @@
-/* eslint-env node */
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
+  env: {
+    // 浏览器环境
+    browser: true,
+    // Node环境
+    node: true,
+    // 启用除了modules以外的所有 ECMAScript 6 特性
+    es2021: true,
+  },
   root: true,
   'extends': [
+    './.eslintrc-auto-import.json',
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier/skip-formatting',
   ],
-  overrides: [
-    {
-      files: [
-        'cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}',
-        'cypress/support/**/*.{js,ts,jsx,tsx}'
-      ],
-      'extends': [
-        'plugin:cypress/recommended'
-      ]
-    }
-  ],
+  globals: {
+    defineEmits: "readonly",
+    defineProps: "readonly",
+    defineExpose: "readonly",
+    withDefaults: "readonly",
+  },
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  rules: {
+    semi: ["warn", "never"], // 禁止尾部使用分号
+    "no-debugger": "warn", // 禁止出现 debugger
+  },
 }
