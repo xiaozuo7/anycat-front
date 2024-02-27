cd147a2735d7a17411a49f1e1c837a6275bb7344
xiaozuo7 <893985729@qq.com>
2024-02-27 17:37:26 +0800

    [init]

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,16 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import path from 'path'
-const pathSrc = path.resolve(__dirname, 'src')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+  ],
   resolve: {
     alias: {
-      '@': pathSrc,
+      '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   }
 })
