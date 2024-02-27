309b1dd067c2eb8bef78db395128f2803b60189f
xiaozuo7 <893985729@qq.com>
2024-02-27 10:20:07 +0800

    [init]vue3+vite+ts

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import path from 'path'
+const pathSrc = path.resolve(__dirname, 'src')
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': pathSrc,
+    }
+  }
+})
