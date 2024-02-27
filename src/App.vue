03bf6d31aea1f42d819e7fa1086a2648f48bbde1
xiaozuo7 <893985729@qq.com>
2024-02-27 10:19:39 +0800

    [init]vue3+vite+ts

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,30 @@
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
 <template>
   <div>
-    <input v-model="inputText" placeholder="请输入要加密的内容" />
-    <button @click="encrypt">加密</button>
-    <div>
-      <textarea v-model="outputText" placeholder="加密后的结果会显示在这里" />
-    </div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
   </div>
+  <HelloWorld msg="Vite + Vue" />
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-      inputText: '',
-      outputText: ''
-    };
-  },
-  methods: {
-    async encrypt() {
-      try {
-        const response = await this.$axios.post('http://139.159.249.79/api/v1/base_encode', {
-          content: this.inputText
-        });
-        this.outputText = response.data.data;
-      } catch (error) {
-        console.error('加密请求出错', error);
-      }
-    }
-  }
-};
-</script>
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
