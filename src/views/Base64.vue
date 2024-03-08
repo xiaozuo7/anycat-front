ad4dbec98640cf40a900af734b9e0972c0e15fa8
xiaozuo7 <893985729@qq.com>
2024-03-08 14:39:31 +0800

    [feat]add base64

--- src/views/Base64.vue
+++ src/views/Base64.vue
@@ -1,4 +1,61 @@
 <template>
-    <div>Base64加解密</div>
-  </template>
-  
+  <div>
+    <div class="title">Base64加解密</div>
+    <a-textarea
+      class="text1"
+      placeholder="请输入要进行编码或解码的字符"
+      allow-clear
+      v-model="text1"
+    />
+    <div align="start" class="space-content">
+      <a-button type="primary" @click="encode">编码</a-button>
+      <a-button @click="decode">解码</a-button>
+
+      <a-button @click="exchange" class="exchange"
+        ><template #icon><icon-system-uicons:chevron-open /></template>交换</a-button
+      >
+    </div>
+    <a-textarea class="text2" placeholder="编码或解码的结果" allow-clear v-model="text2" />
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const text1 = ref('')
+const text2 = ref('')
+
+function encode() {
+  text2.value = window.btoa(text1.value)
+}
+function decode() {
+  text2.value = window.atob(text1.value)
+}
+function exchange() {
+  const x = text2.value
+  text2.value = text1.value
+  text1.value = x
+}
+</script>
+
+<style scoped>
+.text1,
+.text2 {
+  width: 600px;
+  height: 200px;
+}
+.space-content {
+  width: 600px;
+  height: 50px;
+  display: flex;
+  gap: 10px;
+  align-items: center;
+}
+.exchange {
+  margin-left: 100px;
+}
+.title {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 10px;
+}
+</style>
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="w-full flex justify-center items-center flex-1">
-    <div class="w-full h-300px flex justify-center items-center">
+    <div class="w-full h-300px flex justify-center items-center flex-col">
+     
       <div
         class="w-150px h-150px rounded-[50%] bg-[var(--color-fill-1)] flex justify-center items-center"
       >
-        <icon-maki:animal-shelter class="text-52px" />
+        <icon-maki:animal-shelter class="text-42px" />
       </div>
+       <div class="text-18px mt-20px">万能猫-你的万能小帮手</div>
     </div>
   </div>
 </template>
